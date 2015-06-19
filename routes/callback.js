d9ce215b31f57abdc828fbfdc7811ee77f58ae3b
Tyler Krpata <tyler.krpata@gmail.com>
2015-06-19 10:15:47 -0400

    Only save the plain text part

--- routes/callback.js
+++ routes/callback.js
@@ -4,7 +4,8 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
     var db = req.db;
     var collection = db.get('sapphire');
-    collection.insert(req.body, function(err, result){
+    var input = JSON.parse(req.body);
+    collection.insert(input.plain, function(err, result){
         res.send(
             (err === null) ? { msg: '' } : { msg: err }
         );
