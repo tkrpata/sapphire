96bbe7ead0651d73f919cf6f9f84a8eef59fb0e3
Tyler Krpata <tyler.krpata@gmail.com>
2015-06-19 10:51:11 -0400

    Parse out important data

--- routes/callback.js
+++ routes/callback.js
@@ -4,8 +4,27 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
     var db = req.db;
     var collection = db.get('sapphire');
-    console.log(req.body.plain);
-    collection.insert(req.body.plain, function(err, result){
+    var content = req.body.plain;
+    console.log(content);
+
+    var re = /Date: (.*?)$/gm;
+    var date = re.exec(content)[1];
+    console.log(date);
+
+    var re = /attacked by (.*?)$/gm;
+    var frog = re.exec(content)[1];
+    console.log(frog);
+
+    var re = /DAMAGE REPORT\n(.*?)\n(.*?)\n/gm;
+    var location = re.exec(content);
+    var portal = location[1];
+    var address = location[2];
+    console.log(portal);
+    console.log(address);
+
+    var entry = { 'date': date, 'player': frog, 'portal': portal, 'address': address };
+    
+    collection.insert(entry, function(err, result){
         res.send(
             (err === null) ? { msg: '' } : { msg: err }
         );
