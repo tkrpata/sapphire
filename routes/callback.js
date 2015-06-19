22eed4a525fdec2e558e229f27905f6981db6eb7
Tyler Krpata <tyler.krpata@gmail.com>
2015-06-19 12:03:13 -0400

    Geocode the lat and lng

--- routes/callback.js
+++ routes/callback.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 router.post('/', function(req, res, next) {
     var db = req.db;
@@ -27,7 +28,21 @@ router.post('/', function(req, res, next) {
       console.log(portal);
       console.log(address);
 
-      var entry = { 'date': date, 'player': frog, 'portal': portal, 'address': address };
+      var geocode_url = "http://maps.google.com/maps/api/geocode/json?address=" + encodeURIComponent(address) + "&sensor=false";
+      var lat = 0.0;
+      var lng = 0.0;
+      request(geocode_url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          //console.log(body) 
+          obj = JSON.parse(body);
+          lat = obj.results[0].geometry.location.lat;
+          lng = obj.results[0].geometry.location.lng;
+          console.log(lat + "," + lng);
+          
+        }
+      })
+
+      var entry = { 'date': date, 'player': frog, 'portal': portal, 'address': address, 'lat':lat, 'lng':lng };
     
       collection.insert(entry, function(err, result){
         res.send(
@@ -36,10 +51,11 @@ router.post('/', function(req, res, next) {
       });
     } else if (subject.match("Forwarding Confirmation")) {
       console.log(subject);
+      res.send();
     } else {
       console.log("Got an unknown message type from " + req.body.headers.From)
+      res.send();
     }
-    res.send();
 });
 
 module.exports = router;
