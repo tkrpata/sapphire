f70ac7f7c088c8f556b628bfda563854971159ed
Tyler Krpata <tyler.krpata@gmail.com>
2015-06-19 11:06:46 -0400

    Semi-handle gmail forwarding confirmations

--- routes/callback.js
+++ routes/callback.js
@@ -4,31 +4,40 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
     var db = req.db;
     var collection = db.get('sapphire');
+
     var content = req.body.plain;
     console.log(content);
 
-    var re = /Date: (.*?)$/gm;
-    var date = re.exec(content)[1];
-    console.log(date);
+    var subject = req.headers.Subject;
 
-    var re = /attacked by (.*?)$/gm;
-    var frog = re.exec(content)[1];
-    console.log(frog);
+    if(subject.match("Damage Report")) {
+      var re = /Date: (.*?)$/gm;
+      var date = re.exec(content)[1];
+      console.log(date);
 
-    var re = /DAMAGE REPORT\n(.*?)\n(.*?)\n/gm;
-    var location = re.exec(content);
-    var portal = location[1];
-    var address = location[2];
-    console.log(portal);
-    console.log(address);
+      var re = /attacked by (.*?)$/gm;
+      var frog = re.exec(content)[1];
+      console.log(frog);
 
-    var entry = { 'date': date, 'player': frog, 'portal': portal, 'address': address };
+      var re = /DAMAGE REPORT\n(.*?)\n(.*?)\n/gm;
+      var location = re.exec(content);
+      var portal = location[1];
+      var address = location[2];
+      console.log(portal);
+      console.log(address);
+
+      var entry = { 'date': date, 'player': frog, 'portal': portal, 'address': address };
     
-    collection.insert(entry, function(err, result){
+      collection.insert(entry, function(err, result){
         res.send(
             (err === null) ? { msg: '' } : { msg: err }
         );
-    });
+      });
+    } else if (subject.match("Gmail Forwarding Confirmation")) {
+      console.log(subject);
+    } else {
+      console.log("Got an unknown message type from " + req.headers.From)
+    }
 });
 
 module.exports = router;
